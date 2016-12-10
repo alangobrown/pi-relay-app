cd336e269294a681434fe947a61e5376be39c1d5
Alan Brown <alan@mailmob.co.uk>
2016-12-10 20:57:36 +0000

    initial commit

---
+++ settings-default.py
@@ -0,0 +1,9 @@
+#Settings
+
+firebase = {
+  "apiKey": "AAAAA",
+  "authDomain": "AAAAA.firebaseapp.com",
+  "databaseURL": "https://AAAAA.firebaseio.com/",
+  "storageBucket": "projectId.appspot.com",
+  "serviceAccount": "config/firebaseCredentials.json"
+}
