09f56a23af57eace517a62e8bdfdd67780348dd5
Alan Brown <alan@mailmob.co.uk>
2016-12-10 20:56:23 +0000

    initial commit

--- settings-default.py
+++
@@ -1,9 +0,0 @@
-#Settings
-
-firebase = {
-  "apiKey": "AIzaSyAIQIGNFXAzjjWyFqy7FjxERAyU8XyJ9z4",
-  "authDomain": "pi-relay.firebaseapp.com",
-  "databaseURL": "https://pi-relay.firebaseio.com/",
-  "storageBucket": "projectId.appspot.com",
-  "serviceAccount": "config/firebaseCredentials.json"
-}
